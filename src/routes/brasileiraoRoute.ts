084490126e52abb8f256ee0a4267a8007fef7a79
João Rafael Menezes <menezes.jrafael@gmail.com>
2024-03-07 11:28:47 -0300

    alterações nas rotas, deixando algumas publicas temporariamente

--- src/routes/brasileiraoRoute.ts
+++ src/routes/brasileiraoRoute.ts
@@ -7,7 +7,7 @@ import { validadeNewAccountSchema, validateAuthorization } from './accountSchema
 const router = Router();
 
 router.get('/', clubesController.getConnectTest);
-router.get('/clubes', validateAuthorization, clubesController.getClubes);
+router.get('/clubes', clubesController.getClubes);
 router.get('/clube/:idClube', validateAuthorization, clubesController.getClube);
 router.get('/datasJogo', validateAuthorization, datasJogosController.getDatasJogos);
 router.get('/jogosDaRodada/:rodada', validateAuthorization, datasJogosController.getJogosDaRodada);
@@ -17,7 +17,7 @@ router.get('/findAllAccounts', validateAuthorization, accountController.findAll)
 router.get('/findClubByName/:nomeClube', validateAuthorization, clubesController.getClubByName);
 router.post('/addAccount/', validadeNewAccountSchema, validateAuthorization, accountController.addAccount);
 router.post('/accounts/login', accountController.loginAccount);
-router.post('/addJogosDaRodada', validateAuthorization, datasJogosController.postAddJogosRodada);
+router.post('/addJogosDaRodada', datasJogosController.postAddJogosRodada);
 router.post('/addClub', validateAuthorization, clubesController.addClub);
 router.patch('/setScoreGame', validateAuthorization, datasJogosController.setScoreGame);
 
